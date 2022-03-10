290751884a0537bc684facd5116b9f0285c57b97
michele camera <michy.cam95@gmail.com>
2022-03-10 14:44:58 +0100

    first exercise done

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,6 +1,28 @@
 console.log("JS-OK")
 
-// Email check
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+const list = ['michele']
+
+const username = prompt("inserisci il nome")
+
+for (let i = 0; i < list.length; i++) {
+
+    const listparty = list[i];
+
+    if (username === listparty) {
+
+        console.log("You can join!")
+
+    } else {
+        console.log("You can't join!")
+    }
+}
+
+
+
+
+
 
 
 
