c3d467cb70fac077c3e3c21180aabeda412cbd06
michele camera <michy.cam95@gmail.com>
2022-03-10 15:18:20 +0100

    creation second exercise

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -1,7 +1,21 @@
 console.log("JS-OK")
 
-// Email check
+// Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
 
+const validNumbersArray = [];
+
+while (validNumbersArray < 6) {
+
+    const firstnumb = parseInt(prompt('Insert a number!'));
+
+    if (!isNaN(firstnumb) && firstnumb % 2 !== 0) {
+
+        validNumbersArray.push(firstnumb);
+
+        console.log(validNumbersArray)
+    }
+
+}
 
 
 
