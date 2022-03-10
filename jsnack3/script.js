90dbc985967961465f1b4db7221c32e5999102ac
michele camera <michy.cam95@gmail.com>
2022-03-10 16:43:54 +0100

    creation third exercise

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,7 +1,18 @@
 console.log("JS-OK")
 
-// Email check
 
+//Chiedi un numero di 4 cifre all’utente e calcola la somma di tutte le cifre che compongono il numero.
+
+let value = parseInt(prompt("inserisci un numero a quattro cifre!"))
+sum = 0;
+
+while (value) {
+    sum += value % 10;
+    value = Math.floor(value / 10);
+    const number = true;
+}
+
+console.log(sum);
 
 
 
