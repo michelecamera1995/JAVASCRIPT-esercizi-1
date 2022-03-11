fff6bc78e72a9b46954aaeac3b33409a1dc5461a
michele camera <michy.cam95@gmail.com>
2022-03-11 09:49:15 +0100

    modify snack 1

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -2,24 +2,28 @@ console.log("JS-OK")
 
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
-const list = ['michele', 'luca', 'silvio']
+const list = ['michele', 'luca', 'silvio'];
 
-const username = prompt("inserisci il nome")
+const username = prompt("inserisci il nome");
 
-for (let i = 0; i <= list.length; i++) {
+let youcanjoin = false;
 
+for (let i = 0; i < list.length; i++) {
     const listparty = list[i];
-
     if (username === listparty) {
-
-        console.log("You can join!")
-
-    } else {
-
-        console.log("You can't join!")
-
+        youcanjoin = true;
     }
 }
+
+if (youcanjoin) {
+    console.log("You can join!")
+} else {
+    console.log("You can't join!")
+}
+
+
+
+
 
 
 
