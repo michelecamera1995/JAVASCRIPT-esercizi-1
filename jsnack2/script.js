d6a44feaaf0a5bd983d313c6ef88a35060a1ea05
michele camera <michy.cam95@gmail.com>
2022-03-11 09:53:34 +0100

    modify snack 2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -3,19 +3,13 @@ console.log("JS-OK")
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
 
 const numberlist = [];
-
 for (let i = 0; i <= 5; i++) {
-
     const firstnumb = parseInt(prompt('Insert a number!'));
-
     if (!isNaN(firstnumb) && firstnumb % 2 !== 0) {
-
         numberlist.push(firstnumb);
     }
-
-    console.log(numberlist)
-
 }
 
+console.log(numberlist)
 
 
